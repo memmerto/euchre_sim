dae26441ef688bd77475d2c0bf6cb420259e4636
pgwhalen <pgwhalen@gmail.com>
2014-01-28 22:28:33 -0600

    i learned about properties

--- euchre.py
+++ euchre.py
@@ -11,9 +11,10 @@ class Game:
 	def __init__(self, players):
 		if len(players) != 4:
 			raise Exception("Game only supports 5 players")
-		self.__players = players
+		self._players = players
+
 		position = 0
-		for p in self.__players:
+		for p in self._players:
 			p.game = self
 			p.position = position
 			if p.position % 2 == 0:
@@ -22,13 +23,13 @@ class Game:
 				p.team_num = 2
 			position += 1
 
-		self.team_one_score = 0
-		self.team_two_score = 0
-		self.team_one_tricks = 0
-		self.team_two_tricks = 0
+		self._team_one_score = 0
+		self._team_two_score = 0
+		self._team_one_tricks = 0
+		self._team_two_tricks = 0
 		self.__deck = None
-		self.top_card = None
-		self.trump = None
+		self._top_card = None
+		self._trump = None
 
 	def play_game(self):
 		while (self.team_one_score < 10 and self.team_two_score < 10):
@@ -38,12 +39,12 @@ class Game:
 
 	def play_hand(self):
 		# order players based on position values (3 for dealer)
-		self.__players = sorted(self.__players, key=lambda x: x.position)
+		self._players = sorted(self._players, key=lambda x: x.position)
 
 		# deal
 		self.deal_hand()
 		self.print_hand()
-		print "top card", self.top_card
+		print "top card", self._top_card
 
 		# call trump
 		self.call_trump()
@@ -55,7 +56,7 @@ class Game:
 			action_pos = start_pos
 			trick = []
 			for _ in xrange(4):
-				player = self.__players[action_pos % 4]
+				player = self._players[action_pos % 4]
 				if player.active:
 					player.action(trick)
 				
@@ -65,9 +66,13 @@ class Game:
 
 		# score
 		# TODO
+		self._team_one_tricks = 0
+		self._team_two_tricks = 0
+		self._team_one_score = 999
+		self._team_two_score = 999
 
 		# rotate dealer (rotate positions)
-		for p in self.__players:
+		for p in self._players:
 			if p.position == 0:
 				p.position = 3
 			else:
@@ -76,71 +81,95 @@ class Game:
 	def deal_hand(self):
 		self.__deck = [val + suit for val in VALUES for suit in SUITS]
 		shuffle(self.__deck)
-		self.trump = None
+		self._trump = None
 
 		# euchre style dealing, for true authenticity
-		for p in self.__players:
+		for p in self._players:
 			for _ in xrange(randrange(1,5)):
 				p.receive_card(self.__deck.pop())
 
-		for p in self.__players:
+		for p in self._players:
 			for _ in xrange(5-len(p.hand)):
 				p.receive_card(self.__deck.pop())
 
-		self.top_card = self.__deck.pop()
+		self._top_card = self.__deck.pop()
 
 	def call_trump(self):
-		dealer = self.__players[3]
+		dealer = self._players[3]
 
-		for p in self.__players:
-			call_result = p.call(self.top_card)
+		for p in self._players:
+			call_result = p.call(self._top_card)
 			if call_result != False:
-				dealer.receive_card(self.top_card)
+				dealer.receive_card(self._top_card)
 				discard = dealer.discard()
 
 				# TODO: check that an appropriate card is discarded
 				dealer.hand.remove(discard)
 				
-				self.trump = self.top_card[1]
+				self._trump = self._top_card[1]
 				if call_result == "alone":
-					self.__teammate_for(p).active = False
+					self._teammate_for(p).active = False
 
 				# tell players who called
-				for pl in self.__players:
-					pl.end_call(p.position, self.trump)
+				for pl in self._players:
+					pl.end_call(p.position, self._trump)
 				return
-			print p.name, ":", self.trump
+			print p.name, ":", self._trump
 
 		self.print_hand()
 
-		for p in self.__players:
+		for p in self._players:
 			call_result = p.call(None)
 
 			if call_result not in SUITS and p.position == 3:
 				raise Exception("The dealer got screwed - You have to call something!")
-			if call_result == self.top_card[1]:
+			if call_result == self._top_card[1]:
 				raise Exception("Can't call the face up card after it's flipped")
 			if call_result in SUITS:
-				self.trump = call_result
-				for pl in self.__players:
-					pl.end_call(p.position, self.trump)
+				self._trump = call_result
+				for pl in self._players:
+					pl.end_call(p.position, self._trump)
 				return
 
 	def print_hand(self):
 		""" Print hand for each player """
-		print "------------------- Trump:", self.trump, "---------------"
-		for p in self.__players:
+		print "------------------- Trump:", self._trump, "---------------"
+		for p in self._players:
 			if p.active:
 				print p.position, p.name, p.hand
 			else:
 				print p.position, p.name, "*** asleep ***"
 
-	def __teammate_for(self, player):
+	def _teammate_for(self, player):
 		return filter(lambda teammate: teammate.team_num == player.team_num and
 									   teammate.position != player.position,
-									   self.__players)[0]
+									   self._players)[0]
 
-	def 
+	# Properties
+	@property
+	def top_card(self):
+		return self._top_card
+
+	@property
+	def trump(self):
+		return self._trump
+
+	@property
+	def team_one_tricks(self):
+		return self._team_one_tricks
+
+	@property
+	def team_two_tricks(self):
+		return self._team_two_tricks
+
+	@property
+	def team_one_score(self):
+		return self._team_one_score
+
+	@property
+	def team_two_score(self):
+		return self._team_two_score
+
 
 class Player:
 
@@ -158,7 +187,6 @@ class Player:
 		trick -- list of cards played in order
 
 		"""
-		print self.name, "action", self.game.top_card
 		return False
 
 	def call(self, top_card):
@@ -191,14 +219,13 @@ class Player:
 		""" Communicate result of calling to player
 
 		"""
-		print self.name, caller_position, trump
-		pass
+		return
 
 	def end_trick(self):
 		""" Communicate result of trick to player
 
 		"""
-		pass
+		return
 
 	def receive_card(self, card):
 		self.hand.append(card)
