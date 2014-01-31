93190f7e02cf9cfb4ea67a5e14f5ef9023724c13
pgwhalen <pgwhalen@gmail.com>
2014-01-31 16:49:02 -0600

    my_player skeleton

---
+++ my_player.py
@@ -0,0 +1,27 @@
+import util
+from player import Player
+
+class MyPlayer(Player):
+
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def action(self, trick):
+		""" Play a card in trick """
+		pass
+
+	def call(self, top_card):
+		""" Call trump or pass """
+		pass
+
+	def discard(self):
+		""" Choose card to discard after picking up	"""
+		pass
+
+	def end_call(self, caller_position, trump):
+		""" Communicate result of calling to player """
+		pass
+
+	def end_trick(self):
+		""" Communicate result of trick to player """
+		pass
